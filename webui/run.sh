805edac6a8855035d9b6ce2c8186b3e4d5a70f9e
Narūnas Krasauskas <narun4sk@gmail.com>
2017-05-22 22:57:09 +0100

    Do not create docker VOLUMES on build

--- webui/run.sh
+++ webui/run.sh
@@ -2,12 +2,13 @@
 
 NAME="${1:-webui}"
 REV="${2:-latest}"
+APP="$(dirname $(readlink -e $0))/app"
 
 docker run\
  --init\
  --rm\
  --publish 8000:8000\
- --volume app:/opt/"${NAME}"\
+ --volume "${APP}:/opt/${NAME}"\
  --interactive\
  --tty\
  "${NAME}:${REV}"
